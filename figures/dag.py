bc64e98157a5d8886dafde42bc633c88fb26e4cf
Rodrigo Luger <rodluger@gmail.com>
2021-07-15 16:09:04 -0400

    more text

--- figures/dag.py
+++ figures/dag.py
@@ -1,8 +1,25 @@
 import subprocess
 from pathlib import Path
+import os
 
+# Top level
 ROOT = Path(__file__).parents[1].absolute()
-ps = subprocess.Popen(("snakemake", "--dag"), stdout=subprocess.PIPE, cwd=ROOT)
+
+# Generate the DAG. This is in general a terrible idea -- we shouldn't
+# be calling Snakemake within a Snakemake build! But hey, it works.
+dag = subprocess.check_output(["snakemake", "--dag"], cwd=ROOT).decode()
+
+# Trim the file names to reduce the image width
+dag = dag.replace("figure: figures/", "")
+dag = dag.replace("test: tests/", "")
+
+# Save to a temporary file
+with open("dag.dag", "w") as f:
+    print(dag, file=f)
+
+# Generate the PDF
 with open("dag.pdf", "wb") as f:
-    f.write(subprocess.check_output(("dot", "-Tpdf"), stdin=ps.stdout))
-ps.wait()
+    f.write(subprocess.check_output(["dot", "-Tpdf", "dag.dag"]))
+
+# Remove the tempfile
+os.remove("dag.dag")
