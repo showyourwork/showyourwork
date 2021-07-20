d9a7603dc33920ea7f17be37042b82b88a12e298
Rodrigo Luger <rodluger@gmail.com>
2021-07-20 19:54:16 -0400

    deprecate user stuff

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "write_to": Path("showyourwork") / "showyourwork_version.py",
         "write_to_template": '__version__ = "{version}"\n',
     },
-    install_requires=["setuptools_scm", "questionary", "numpy"],
+    install_requires=["setuptools_scm", "numpy"],
     entry_points={
         "console_scripts": ["showyourwork=showyourwork.entry_points:main"]
     },
