a5b98774ee1be773f9ace6d12736ad766e867deb
Rodrigo Luger <rodluger@gmail.com>
2021-07-20 15:45:34 -0400

    better defaults

--- showyourwork/meta/user.py
+++ showyourwork/meta/user.py
@@ -1,24 +1,76 @@
 from ..constants import *
 from ..utils import save_json
 import questionary
+import subprocess
 import json
 import os
+
+
+__all__ = ["get_user_metadata"]
+
+
+def get_user_name():
+    name = os.getenv("USER_EMAIL", "Author Name")
+    if name == "Author Name":
+        try:
+            name = (
+                subprocess.check_output(
+                    ["git", "log", "--format='%an'", "HEAD^!"],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                    cwd=USER,
+                )
+                .decode()
+                .replace("\n", "")
+            )
+        except Exception as e:
+            pass
+    return name
+
+
+def get_user_email():
+    email = os.getenv("USER_EMAIL", "author@email")
+    if email == "author@email":
+        try:
+            email = (
+                subprocess.check_output(
+                    ["git", "log", "--format='%ae'", "HEAD^!"],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                    cwd=USER,
+                )
+                .decode()
+                .replace("\n", "")
+            )
+        except Exception as e:
+            pass
+    return email
+
+
+def get_user_github():
+    return os.getenv("GITHUB_REPOSITORY", "unknown/").split("/")[0]
 
 
 def get_user_metadata(clobber=True):
 
     if clobber or not (TEMP / "user" / "user.json").exists():
 
-        # If we're on GitHub Actions, disable user prompts
         if os.getenv("GITHUB_ACTIONS", None) is not None:
 
             user = {
-                "name": "First Author",
-                "email": "email@website.com",
-                "affil": "First Author Institution",
-                "orcid": "0000-0000-0000-0000",
-                "github": "github_handle",
+                "name": "",
+                "email": "",
+                "affil": "",
+                "orcid": "",
+                "github": "",
             }
+
+            # Use GitHub Actions settings
+            user["name"] = get_user_name()
+            user["email"] = get_user_email()
+            user["affil"] = os.getenv("USER_AFFIL", "Author Affiliation")
+            user["orcid"] = os.getenv("USER_ORCID", "0000-0000-0000-0000")
+            user["github"] = get_user_github()
 
         else:
 
