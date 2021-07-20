959630c9431b4a053eabe2e72579d19e6a26a651
Rodrigo Luger <rodluger@gmail.com>
2021-07-20 10:38:03 -0400

    disable questionary on actions

--- showyourwork/meta/user.py
+++ showyourwork/meta/user.py
@@ -2,63 +2,77 @@ from ..constants import *
 from ..utils import save_json
 import questionary
 import json
+import os
 
 
 def get_user_metadata(clobber=True):
 
     if clobber or not (TEMP / "user" / "user.json").exists():
 
-        # Custom style
-        style = questionary.Style([])
+        # If we're on GitHub Actions, disable user prompts
+        if os.getenv("GITHUB_ACTIONS", None) is not None:
 
-        # Defaults
-        if (TEMP / "user" / "user.json").exists():
-            with open(TEMP / "user" / "user.json", "r") as f:
-                user = json.load(f)
-        else:
             user = {
-                "name": "",
-                "email": "",
-                "affil": "",
-                "orcid": "",
-                "github": "",
+                "name": "First Author",
+                "email": "email@website.com",
+                "affil": "First Author Institution",
+                "orcid": "0000-0000-0000-0000",
+                "github": "github_handle",
             }
 
-        # Command-line questionnaire
-        user["name"] = questionary.text(
-            "Full Name:",
-            qmark="[ShowYourWork!]",
-            default=user["name"],
-            style=style,
-        ).ask()
+        else:
 
-        user["email"] = questionary.text(
-            "Email:",
-            qmark="[ShowYourWork!]",
-            default=user["email"],
-            style=style,
-        ).ask()
+            # Custom style
+            style = questionary.Style([])
 
-        user["affil"] = questionary.text(
-            "Affiliation:",
-            qmark="[ShowYourWork!]",
-            default=user["affil"],
-            style=style,
-        ).ask()
+            # Defaults
+            if (TEMP / "user" / "user.json").exists():
+                with open(TEMP / "user" / "user.json", "r") as f:
+                    user = json.load(f)
+            else:
+                user = {
+                    "name": "",
+                    "email": "",
+                    "affil": "",
+                    "orcid": "",
+                    "github": "",
+                }
 
-        user["orcid"] = questionary.text(
-            "ORCID:",
-            qmark="[ShowYourWork!]",
-            default=user["orcid"],
-            style=style,
-        ).ask()
+            # Command-line questionnaire
+            user["name"] = questionary.text(
+                "Full Name:",
+                qmark="[ShowYourWork!]",
+                default=user["name"],
+                style=style,
+            ).ask()
 
-        user["github"] = questionary.text(
-            "GitHub Handle:",
-            qmark="[ShowYourWork!]",
-            default=user["github"],
-            style=style,
-        ).ask()
+            user["email"] = questionary.text(
+                "Email:",
+                qmark="[ShowYourWork!]",
+                default=user["email"],
+                style=style,
+            ).ask()
+
+            user["affil"] = questionary.text(
+                "Affiliation:",
+                qmark="[ShowYourWork!]",
+                default=user["affil"],
+                style=style,
+            ).ask()
+
+            user["orcid"] = questionary.text(
+                "ORCID:",
+                qmark="[ShowYourWork!]",
+                default=user["orcid"],
+                style=style,
+            ).ask()
+
+            user["github"] = questionary.text(
+                "GitHub Handle:",
+                qmark="[ShowYourWork!]",
+                default=user["github"],
+                style=style,
+            ).ask()
 
         # Save
         save_json(user, TEMP / "user" / "user.json")
