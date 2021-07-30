c3000cf5a99890860895a538882dade6a049286e
Rodrigo Luger <rodluger@gmail.com>
2021-07-29 18:40:54 -0700

    bugfix

--- .github/workflows/generate.sh
+++ .github/workflows/generate.sh
@@ -4,8 +4,8 @@
 rm -rf ~/.cookiecutterrc
 echo "default_context:" >> ~/.cookiecutterrc
 echo "    github_repo: \"${TARGET_REPOSITORY}\"" >> ~/.cookiecutterrc
-echo "    github_user: \"rodluger\"" >> ~/.cookiecutterrc
-echo "    author_name: \"Rodrigo Luger\"" >> ~/.cookiecutterrc
+echo "    github_user: \"${TARGET_USER}\"" >> ~/.cookiecutterrc
+echo "    author_name: \"${AUTHOR_NAME}\"" >> ~/.cookiecutterrc
 [[ ! -z "${ADD_CALLBACK}" ]] && echo "    _source_repo: \"${GITHUB_REPOSITORY}\"" >> ~/.cookiecutterrc
 
 # Create repo and force push to target
@@ -14,5 +14,5 @@ cd ${TARGET_REPOSITORY}
 git init
 git checkout --orphan main
 git add .
-git -c user.name="${GITHUB_REPOSITORY}" -c user.email="${GITHUB_REPOSITORY}" commit -m "auto commit from \"${GITHUB_REPOSITORY}\""
-git push --force https://x-access-token:${ACCESS_TOKEN}@github.com/${TARGET_REPOSITORY} main
+git -c user.name="${GITHUB_REPOSITORY}" -c user.email="${GITHUB_REPOSITORY}" commit -m "auto commit from ${GITHUB_REPOSITORY}"
+git push --force https://x-access-token:${ACCESS_TOKEN}@github.com/${TARGET_USER}/${TARGET_REPOSITORY} main
