9b5477f7f49003adab4ec7dc5b315a326c46ceb7
Rodrigo Luger <rodluger@gmail.com>
2021-08-06 19:01:22 -0400

    upgrade uuid

--- node_modules/@azure/storage-blob/node_modules/@azure/core-http/types/latest/src/util/url.browser.d.ts
+++
@@ -1,8 +0,0 @@
-declare const url: {
-    new (url: string, base?: string | URL | undefined): URL;
-    prototype: URL;
-    createObjectURL(object: any): string;
-    revokeObjectURL(url: string): void;
-};
-export { url as URL };
-//# sourceMappingURL=url.browser.d.ts.map
