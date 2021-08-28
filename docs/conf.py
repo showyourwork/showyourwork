c035964807bdc95a290a6db3dd214edf8556377e
Rodrigo Luger <rodluger@gmail.com>
2021-08-28 19:55:44 -0400

    rtd

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@ release = '1.0.0'
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
