66d5dc1bd740bb24af533352a394b261348ab201
Rodrigo Luger <rodluger@gmail.com>
2021-08-31 09:33:06 -0400

    better testing

--- .github/workflows/update_template.sh
+++ .github/workflows/update_template.sh
@@ -7,15 +7,15 @@ set -e
 git clone --recurse-submodules https://github.com/${USER}/${REPO}
 cd ${REPO}/showyourwork
 git fetch --all --tags
-git checkout tags/"${{steps.bump.outputs.result}}"
+git checkout tags/${VERSION}
 cd ..
 git add showyourwork
-git -c user.name='gh-actions' -c user.email='gh-actions' commit -m "update showyourwork to ${{steps.bump.outputs.result}}"
+git -c user.name='gh-actions' -c user.email='gh-actions' commit -m "update showyourwork to ${VERSION}"
 git push https://x-access-token:${ACCESS_TOKEN}@github.com/${USER}/${REPO} main
 
 # Now update the template-test repo; note that
 # we sub in a different YAML file that will run the tests
 cp showyourwork-action/resources/tests.yml .github/workflows/showyourwork.yml
 git add .github/workflows/showyourwork.yml
-git -c user.name='gh-actions' -c user.email='gh-actions' commit -m "update showyourwork to ${{steps.bump.outputs.result}}"
+git -c user.name='gh-actions' -c user.email='gh-actions' commit -m "update showyourwork to ${VERSION}"
 git push --force https://x-access-token:${ACCESS_TOKEN}@github.com/${USER}/${REPO}-test main
