0cec4aa1191e80d61053ad54abab259c8a2c1b76
Rodrigo Luger <rodluger@gmail.com>
2021-11-18 11:42:43 -0500

    upload error logs

--- showyourwork-action/src/onerror.js
+++ showyourwork-action/src/onerror.js
@@ -2,6 +2,7 @@
 const core = require("@actions/core");
 const artifact = require("@actions/artifact");
 const shell = require("shelljs");
+const glob = require("glob");
 
 // Exports
 module.exports = { uploadTemporaries };
@@ -12,20 +13,40 @@ module.exports = { uploadTemporaries };
  */
 async function uploadTemporaries() {
 
-    // TODO: Assemble a list of files to upload
+    // Maximum size of individual files; files bigger
+    // than this will be skipped.
+    const maxSizeInKB = 5000;
 
-    // Upload an artifact
-    /*
-    const files = {};
+    // Assemble a list of folders/files to upload
+    const patterns = [
+      "*.*",
+      "src/*.*"
+    ];
+    var files = [];
+    patterns.forEach(function (pattern) {
+      glob(pattern, (err, fs) => {
+        files.forEach(function (f) {
+          try {
+            var sz = parseInt(shell.exec(`du -k ${f} | cut -f1`).stdout.replace('\n', ''));
+            if (sz < maxSizeInKB) {
+              files.push(f);
+            }
+          } catch (error) {
+            core.warning(error.message);
+          }
+        })
+      })
+    });
+
+    // Upload the artifact
     const artifactClient = artifact.create();
     const uploadResponse = await artifactClient.uploadArtifact(
       "showyourwork-output", 
       files, 
       ".", 
       {
-        continueOnError: false
+        continueOnError: true
       }
     );
-    */
    
 }
