61d91cd5535d19da403a85db7eda65cdba533ae9
Rodrigo Luger <rodluger@gmail.com>
2021-11-19 16:20:31 -0500

    install graphviz

--- showyourwork-action/src/conda.js
+++ showyourwork-action/src/conda.js
@@ -52,6 +52,10 @@ async function setupConda() {
       "conda install -y -c defaults -c conda-forge -c bioconda mamba==0.17.0 snakemake-minimal jinja2",
       "Install snakemake"
     );
+    exec(
+      "sudo apt install graphviz",
+      "Install graphviz"
+    );
   }
 
   // Save conda cache (failure OK)
