60cd9ba2bf097968347979ebdf64adccf40ab251
Rodrigo Luger <rodluger@gmail.com>
2021-12-21 10:22:01 -0800

    fix for pdflatex

--- workflow/rules/files.py
+++ workflow/rules/files.py
@@ -63,7 +63,7 @@ class files:
         tectonic = []
 
     #: Figures that are allowed directly in the ``src/`` directory
-    special_figures = ["orcid-id.png", "orcid-ID.png"]
+    special_figures = ["orcid-id.png", "orcid-ID.png", "showyourwork.pdf"]
 
     #: Store temporary exception messages
     exception = relpaths.temp / "exception.log"
