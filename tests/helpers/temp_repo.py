26dfcb9980a6a565701df461c74e5804fc314b14
Rodrigo Luger <rodluger@gmail.com>
2022-04-25 10:26:10 -0400

    ci fix

--- tests/helpers/temp_repo.py
+++ tests/helpers/temp_repo.py
@@ -94,19 +94,18 @@ class TemporaryShowyourworkRepository:
             # Enable overleaf syncing
             options += f"--overleaf={self.overleaf_id}"
 
-        # Provide git name & email if missing
-        get_stdout(
-            "git config --global user.name &> /dev/null "
-            "|| git config --global user.name 'gh-actions'",
-            cwd=SANDBOX,
-            shell=True,
-        )
-        get_stdout(
-            "git config --global user.email &> /dev/null "
-            "|| git config --global user.email 'gh-actions'",
-            cwd=SANDBOX,
-            shell=True,
-        )
+        # Provide git name & email
+        if os.getenv("CI", "false") == "true":
+            get_stdout(
+                "git config --global user.name 'gh-actions'",
+                cwd=SANDBOX,
+                shell=True,
+            )
+            get_stdout(
+                "git config --global user.email 'gh-actions'",
+                cwd=SANDBOX,
+                shell=True,
+            )
 
         # Create a new one
         print(
