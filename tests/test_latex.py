84f0020c4893c36a94d445eb2c08d42307bded93
Rodrigo Luger <rodluger@gmail.com>
2022-05-23 17:06:55 -0400

    test latex install

---
+++ tests/test_latex.py
@@ -0,0 +1,78 @@
+from helpers import TemporaryShowyourworkRepository
+
+
+# A workflow that explicitly installs a TeX distribution
+workflow = """
+name: build
+
+on:
+  push:
+  workflow_dispatch:
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    name: Build the article PDF
+    concurrency: showyourwork-${{ github.ref }}
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+        with:
+          fetch-depth: 0
+
+      - name: Install TinyTex for matplotlib LaTeX rendering
+        id: tinytex
+        shell: bash -l {0}
+        run: |
+          wget -qO- "https://yihui.org/tinytex/install-bin-unix.sh" | sh 
+          sudo ~/bin/tlmgr install type1cm cm-super
+          echo "~/bin" >> $GITHUB_PATH
+
+      - name: Test TeX installation
+        id: test-tex
+        shell: bash -l {0}
+        run: |
+          latex --version
+          pdflatex --version
+
+      - name: Build the article PDF
+        id: build
+        uses: showyourwork/showyourwork-action@v1
+        env:
+          ZENODO_TOKEN: ${{ secrets.ZENODO_TOKEN }}
+          SANDBOX_TOKEN: ${{ secrets.SANDBOX_TOKEN }}
+          OVERLEAF_EMAIL: ${{ secrets.OVERLEAF_EMAIL }}
+          OVERLEAF_PASSWORD: ${{ secrets.OVERLEAF_PASSWORD }}
+"""
+
+
+# A figure script that requires LaTeX
+figure_script = """
+import matplotlib.pyplot as pl
+import paths
+fig = plt.figure(figsize=(7, 6))
+plt.plot([0, 1], [0, 1])
+plt.xlabel(r'$\alpha \beta \frac{1}{2} \exp{-x^2}$')
+fig.savefig(paths.figures / 'test_figure.pdf', bbox_inches='tight')
+"""
+
+
+class TestMatplotlibLatex(TemporaryShowyourworkRepository):
+    """Test rendering TeX in matplotlib."""
+
+    def customize():
+        """Add a figure that requires LaTeX to build."""
+        # Edit the workflow
+        with open(self.cwd / ".github" / "workflows" / "build.yml", "w") as f:
+            print(workflow, file=f)
+
+        # Create the figure script
+        with open(self.cwd / "src" / "scripts" / "test_figure.py", "w") as f:
+            print(figure_script, file=f)
+
+        # Add the figure environment to the tex file
+        self.add_figure_environment()
+
+    def build_local():
+        """Disable local build, since this is explicitly a CI test."""
+        pass
