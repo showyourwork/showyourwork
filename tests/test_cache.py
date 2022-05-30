123ae9f1e1cd153b2a66cca2f4c1c0e559221fc7
Rodrigo Luger <rodluger@gmail.com>
2022-05-30 09:15:16 -0700

    properly skip some tests on ci

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -67,123 +67,118 @@ class TestDirCache(
         self.add_figure_environment()
 
 
-@pytest.mark.skipif(
-    os.getenv("CI", "false") == "true",
-    reason="test generates a (semi-)permanent deposit on Zenodo Sandbox, "
-    "so it should not be run too often",
-)
-class TestCacheFreeze(
-    TemporaryShowyourworkRepository, ShowyourworkRepositoryActions
-):
-    """Test the Zenodo cache freeze feature."""
+if os.getenv("CI", "false") != "true":
+    # These tests generate (semi-)permanent deposits on Zenodo Sandbox
+    # so it should not be run too often.
+    # NOTE: I tried using `pytest.mark.skipif` but they were getting run
+    # on CI anyways!
 
-    # Enable caching
-    cache = True
+    class TestCacheFreeze(
+        TemporaryShowyourworkRepository, ShowyourworkRepositoryActions
+    ):
+        """Test the Zenodo cache freeze feature."""
 
-    # No need to test this on CI
-    local_build_only = True
+        # Enable caching
+        cache = True
 
-    def customize(self):
-        """Add all necessary files for the build."""
-        # Add the pipeline script
-        self.add_pipeline_script(seed=0)
+        # No need to test this on CI
+        local_build_only = True
 
-        # Add the Snakefile rule to generate the dataset
-        self.add_pipeline_rule()
+        def customize(self):
+            """Add all necessary files for the build."""
+            # Add the pipeline script
+            self.add_pipeline_script(seed=0)
 
-        # Add the script to generate the figure
-        self.add_figure_script(load_data=True)
+            # Add the Snakefile rule to generate the dataset
+            self.add_pipeline_rule()
 
-        # Make the dataset a dependency of the figure
-        with edit_yaml(self.cwd / "showyourwork.yml") as config:
-            config["dependencies"] = {
-                "src/scripts/test_figure.py": "src/data/test_data.npz"
-            }
+            # Add the script to generate the figure
+            self.add_figure_script(load_data=True)
 
-        # Add the figure environment to the tex file
-        self.add_figure_environment()
+            # Make the dataset a dependency of the figure
+            with edit_yaml(self.cwd / "showyourwork.yml") as config:
+                config["dependencies"] = {
+                    "src/scripts/test_figure.py": "src/data/test_data.npz"
+                }
 
-    def check_build(self):
-        # Freeze the cache for `seed=0`, which publishes the cache on Sandbox
-        get_stdout("showyourwork cache freeze", cwd=self.cwd, shell=True)
+            # Add the figure environment to the tex file
+            self.add_figure_environment()
 
-        # Edit the pipeline script and re-build. This will
-        # update the latest draft of the Sandbox cache
-        self.add_pipeline_script(seed=1)
-        self.git_commit()
-        self.build_local()
+        def check_build(self):
+            # Freeze the cache for `seed=0`, which publishes the cache on Sandbox
+            get_stdout("showyourwork cache freeze", cwd=self.cwd, shell=True)
 
-        # Clean so we delete the local cache
-        get_stdout("showyourwork clean", cwd=self.cwd, shell=True)
+            # Edit the pipeline script and re-build. This will
+            # update the latest draft of the Sandbox cache
+            self.add_pipeline_script(seed=1)
+            self.git_commit()
+            self.build_local()
 
-        # Revert the script to `seed=0` and re-build, telling showyourwork
-        # to raise an exception if the rule actually gets executed.
-        # The expected behavior is for showyourwork to find the frozen version
-        # of the cache and use that to bypass the pipeline script run.
-        self.add_pipeline_script(seed=0)
-        self.git_commit()
-        self.build_local(pre="SYW_NO_RUN=true")
+            # Clean so we delete the local cache
+            get_stdout("showyourwork clean", cwd=self.cwd, shell=True)
 
+            # Revert the script to `seed=0` and re-build, telling showyourwork
+            # to raise an exception if the rule actually gets executed.
+            # The expected behavior is for showyourwork to find the frozen version
+            # of the cache and use that to bypass the pipeline script run.
+            self.add_pipeline_script(seed=0)
+            self.git_commit()
+            self.build_local(pre="SYW_NO_RUN=true")
 
-@pytest.mark.skipif(
-    os.getenv("CI", "false") == "true",
-    reason="test generates a (semi-)permanent deposit on Zenodo Sandbox, "
-    "so it should not be run too often",
-)
-class TestCachePublish(
-    TemporaryShowyourworkRepository, ShowyourworkRepositoryActions
-):
-    """Test the Zenodo cache publish feature."""
+    class TestCachePublish(
+        TemporaryShowyourworkRepository, ShowyourworkRepositoryActions
+    ):
+        """Test the Zenodo cache publish feature."""
 
-    # Enable caching
-    cache = True
+        # Enable caching
+        cache = True
 
-    # No need to test this on CI
-    local_build_only = True
+        # No need to test this on CI
+        local_build_only = True
 
-    def customize(self):
-        """Add all necessary files for the build."""
-        # Add the pipeline script
-        self.add_pipeline_script(seed=0)
+        def customize(self):
+            """Add all necessary files for the build."""
+            # Add the pipeline script
+            self.add_pipeline_script(seed=0)
 
-        # Add the Snakefile rule to generate the dataset
-        self.add_pipeline_rule()
+            # Add the Snakefile rule to generate the dataset
+            self.add_pipeline_rule()
 
-        # Add the script to generate the figure
-        self.add_figure_script(load_data=True)
+            # Add the script to generate the figure
+            self.add_figure_script(load_data=True)
 
-        # Make the dataset a dependency of the figure
-        with edit_yaml(self.cwd / "showyourwork.yml") as config:
-            config["dependencies"] = {
-                "src/scripts/test_figure.py": "src/data/test_data.npz"
-            }
+            # Make the dataset a dependency of the figure
+            with edit_yaml(self.cwd / "showyourwork.yml") as config:
+                config["dependencies"] = {
+                    "src/scripts/test_figure.py": "src/data/test_data.npz"
+                }
 
-        # Add the figure environment to the tex file
-        self.add_figure_environment()
+            # Add the figure environment to the tex file
+            self.add_figure_environment()
 
-    def check_build(self):
-        # Publish the cache for `seed=0`.
-        # This normally publishes the cache on *Zenodo*, but we can hack it
-        # to use Sandbox so we don't create an actual DOI every time we test!
-        get_stdout(
-            "SANDBOX_ONLY=true showyourwork cache publish",
-            cwd=self.cwd,
-            shell=True,
-        )
+        def check_build(self):
+            # Publish the cache for `seed=0`.
+            # This normally publishes the cache on *Zenodo*, but we can hack it
+            # to use Sandbox so we don't create an actual DOI every time we test!
+            get_stdout(
+                "SANDBOX_ONLY=true showyourwork cache publish",
+                cwd=self.cwd,
+                shell=True,
+            )
 
-        # Edit the pipeline script and re-build. This will
-        # update the latest draft of the Sandbox cache
-        self.add_pipeline_script(seed=1)
-        self.git_commit()
-        self.build_local()
+            # Edit the pipeline script and re-build. This will
+            # update the latest draft of the Sandbox cache
+            self.add_pipeline_script(seed=1)
+            self.git_commit()
+            self.build_local()
 
-        # Clean so we delete the local cache
-        get_stdout("showyourwork clean", cwd=self.cwd, shell=True)
+            # Clean so we delete the local cache
+            get_stdout("showyourwork clean", cwd=self.cwd, shell=True)
 
-        # Revert the script to `seed=0` and re-build, telling showyourwork
-        # to raise an exception if the rule actually gets executed.
-        # The expected behavior is for showyourwork to find the frozen version
-        # of the cache and use that to bypass the pipeline script run.
-        self.add_pipeline_script(seed=0)
-        self.git_commit()
-        self.build_local(pre="SYW_NO_RUN=true")
+            # Revert the script to `seed=0` and re-build, telling showyourwork
+            # to raise an exception if the rule actually gets executed.
+            # The expected behavior is for showyourwork to find the frozen version
+            # of the cache and use that to bypass the pipeline script run.
+            self.add_pipeline_script(seed=0)
+            self.git_commit()
+            self.build_local(pre="SYW_NO_RUN=true")
