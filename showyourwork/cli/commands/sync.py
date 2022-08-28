9327059d6f846fa4b4777488207eeb0372a607e2
Dan F-M <foreman.mackey@gmail.com>
2022-08-28 16:02:55 -0600

    Starting to implement frontend

---
+++ showyourwork/cli/commands/sync.py
@@ -0,0 +1,19 @@
+import os
+
+from ... import exceptions, logging, paths
+from ...config import edit_yaml
+from ...git import get_repo_branch
+from ...overleaf2 import Overleaf
+
+
+def overleaf_sync(force):
+    branch = get_repo_branch()
+    with edit_yaml("overleaf.yml") as config:
+        version = config["overleaf"].get("version", 1)
+        assert version == 2
+
+        url = config["overleaf"].get(
+            "url", f"https://git.overleaf.com/{config['overleaf']['id']}"
+        )
+        local_sha = config["overleaf"].get("local_sha", None)
+        remote_sha = config["overleaf"].get("remote_sha", None)
