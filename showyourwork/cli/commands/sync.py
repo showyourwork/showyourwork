7916ffe6bfb5c2a63aad6c8f7c951ff8449e359b
Dan F-M <foreman.mackey@gmail.com>
2022-08-28 21:12:57 -0600

    lint

--- showyourwork/cli/commands/sync.py
+++ showyourwork/cli/commands/sync.py
@@ -1,12 +1,9 @@
-
 from ... import logging, paths
 from ...config import edit_yaml
-from ...git import get_repo_branch
 from ...overleaf2 import Overleaf
 
 
-def overleaf_sync(init=False, force=False):
-    branch = get_repo_branch()
+def overleaf_sync(continue_=False, init=False, force=False):
     with edit_yaml("overleaf.yml") as config:
         version = config["overleaf"].get("version", 1)
         assert version == 2
@@ -16,3 +13,17 @@ def overleaf_sync(init=False, force=False):
         )
         local_sha = config["overleaf"].get("local_sha", None)
         remote_sha = config["overleaf"].get("remote_sha", None)
+
+    overleaf = Overleaf.from_url(
+        url, local_sha=local_sha, remote_sha=remote_sha
+    )
+
+    if init:
+        overleaf.setup_remote(force=force)
+        return
+
+    if continue_:
+        overleaf = overleaf.continue_sync()
+
+    else:
+        overleaf = overleaf.sync(force=force)
