e4a72bbc22e58e51cbdf9255e196290bd52124e1
Rodrigo Luger <rodluger@gmail.com>
2022-09-27 10:15:19 -0400

    back to condarc

--- showyourwork/cli/conda_env.py
+++ showyourwork/cli/conda_env.py
@@ -120,7 +120,7 @@ def run_in_env(command, **kwargs):
             "Creating a new conda environment in ~/.showyourwork/env..."
         )
         get_stdout(
-            f"conda create --strict-channel-priority -p {paths.user().env} --file {workflow_envfile} -q",
+            f"CONDARC={paths.showyourwork().envs / '.condarc'} conda create -p {paths.user().env} --file {workflow_envfile} -q",
             shell=True,
         )
         shutil.copy(workflow_envfile, cached_envfile)
@@ -137,7 +137,7 @@ def run_in_env(command, **kwargs):
         if not cache_hit:
             logger.info("Updating conda environment in ~/.showyourwork/env...")
             get_stdout(
-                f"conda update --strict-channel-priority -p {paths.user().env} --file {workflow_envfile} --prune -q",
+                f"CONDARC={paths.showyourwork().envs / '.condarc'} conda update -p {paths.user().env} --file {workflow_envfile} --prune -q",
                 shell=True,
             )
             shutil.copy(workflow_envfile, cached_envfile)
