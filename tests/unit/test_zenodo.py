4e41e8f1c43add5fe4725a39b23733fe4a6e4818
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-01-28 16:23:14 -0500

    Add test for get_id_type()

--- tests/unit/test_zenodo.py
+++ tests/unit/test_zenodo.py
@@ -1,6 +1,6 @@
 import pytest
 
-from showyourwork import zenodo
+from showyourwork.zenodo import Zenodo, get_dataset_dois, get_dataset_urls
 
 
 @pytest.fixture
@@ -32,29 +32,35 @@ def config():
 
 def test_get_dataset_urls(config):
     # Test with a single good file
-    urls = zenodo.get_dataset_urls(["src/data/TOI640/planet.json"], config["datasets"])
+    urls = get_dataset_urls(["src/data/TOI640/planet.json"], config["datasets"])
     assert len(urls) == 1
     assert urls[0] == "https://zenodo.org/records/6515864"
 
     # Test with part of a zip file
-    urls = zenodo.get_dataset_urls(["src/data/TOI640/S06.json"], config["datasets"])
+    urls = get_dataset_urls(["src/data/TOI640/S06.json"], config["datasets"])
     assert urls[0] == "https://zenodo.org/records/6515864"
 
     # Test with a non-existing file
-    urls = zenodo.get_dataset_urls(["src/data/TOI640/S067.json"], config["datasets"])
+    urls = get_dataset_urls(["src/data/TOI640/S067.json"], config["datasets"])
     assert len(urls) == 0
 
 
 def test_get_dataset_doi(config):
     # Test with a single good file
-    dois = zenodo.get_dataset_dois(["src/data/TOI640/planet.json"], config["datasets"])
+    dois = get_dataset_dois(["src/data/TOI640/planet.json"], config["datasets"])
     assert len(dois) == 1
     assert dois[0] == "10.5281/zenodo.6515864"
 
     # Test with part of a zip file
-    dois = zenodo.get_dataset_dois(["src/data/TOI640/S06.json"], config["datasets"])
+    dois = get_dataset_dois(["src/data/TOI640/S06.json"], config["datasets"])
     assert dois[0] == "10.5281/zenodo.6515864"
 
     # Test with a non-existing file
-    dois = zenodo.get_dataset_dois(["src/data/TOI640/S067.json"], config["datasets"])
+    dois = get_dataset_dois(["src/data/TOI640/S067.json"], config["datasets"])
     assert len(dois) == 0
+
+
+def test_get_id_type(config):
+    doi = next(iter(config["datasets"]))
+    deposit = Zenodo(doi)
+    assert deposit.get_id_type() == "version"
