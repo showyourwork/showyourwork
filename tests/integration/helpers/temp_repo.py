a1e386f9b4f2a0c5d05d7af6e4f0647ed689f630
Alan Correa <40885942+thealanjason@users.noreply.github.com>
2026-01-29 16:41:26 +0100

    Update temp_repo.py

--- tests/integration/helpers/temp_repo.py
+++ tests/integration/helpers/temp_repo.py
@@ -175,7 +175,7 @@ class TemporaryShowyourworkRepository:
                 raise Exception(stdout + "\n" + stderr)
 
         get_stdout(
-            f"{pre} CI=false showyourwork build --conda-frontend mamba",
+            f"{pre} CI=false showyourwork build",
             shell=True,
             cwd=self.cwd,
             callback=callback,
