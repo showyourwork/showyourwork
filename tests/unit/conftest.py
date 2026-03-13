1f6bab02ed3a2f73fc6879382ccf1d9c7afa6a16
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-03-13 16:15:13 -0400

    Add CLI flag to force zenodo access

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -3,6 +3,16 @@ import os
 import pytest
 
 
+def pytest_addoption(parser):
+    """Add custom pytest options."""
+    parser.addoption(
+        "--require-zenodo",
+        action="store_true",
+        default=False,
+        help="Force zenodo tests to run even if tokens are not set",
+    )
+
+
 def pytest_configure(config):
     """Register custom pytest markers."""
     config.addinivalue_line(
@@ -12,11 +22,14 @@ def pytest_configure(config):
 
 
 def pytest_collection_modifyitems(config, items):
-    """Skip zenodo tests if SANDBOX_TOKEN is not set."""
+    """
+    Skip zenodo tests if SANDBOX_TOKEN is not set (unless --require-zenodo is passed).
+    """
     sandbox_token = os.getenv("SANDBOX_TOKEN")
     zenodo_token = os.getenv("ZENODO_TOKEN")
+    require_zenodo = config.getoption("--require-zenodo")
 
-    if not sandbox_token or not zenodo_token:
+    if not require_zenodo and (not sandbox_token or not zenodo_token):
         skipper = pytest.mark.skip(
             reason="SANDBOX_TOKEN or ZENODO_TOKEN environment variable not set"
         )
