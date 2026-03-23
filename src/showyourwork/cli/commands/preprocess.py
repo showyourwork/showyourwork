331a215517fa87d35d5219d880d2127e10cdd294
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-03-23 17:23:49 -0400

    Filter out target argumnent from snakemake preprocessing

--- src/showyourwork/cli/commands/preprocess.py
+++ src/showyourwork/cli/commands/preprocess.py
@@ -1,3 +1,5 @@
+from snakemake.cli import parse_args
+
 from ... import paths
 from .run_snakemake import run_snakemake
 
@@ -12,11 +14,16 @@ def preprocess(snakemake_args=(), cores=1, conda_frontend="conda"):
         arg for arg in snakemake_args if arg not in ["--dry-run", "-n"]
     )
     snakefile = paths.showyourwork().workflow / "prep.smk"
+    target_args = parse_args(snakemake_args)[1].targets
+    snakemake_args_notargets = []
+    for arg in snakemake_args:
+        if arg not in target_args:
+            snakemake_args_notargets.append(arg)
     run_snakemake(
         snakefile.as_posix(),
         run_type="preprocess",
         cores=cores,
         conda_frontend=conda_frontend,
-        extra_args=snakemake_args,
+        extra_args=snakemake_args_notargets,
         check=True,
     )
