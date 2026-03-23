dddc49b9a1dc59767e41ba88d1da2a958c33dca6
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-03-23 17:42:31 -0400

    Add integration test with two target rules

--- tests/integration/test_target.py
+++ tests/integration/test_target.py
@@ -1,7 +1,8 @@
 from helpers import ShowyourworkRepositoryActions, TemporaryShowyourworkRepository
 
 
-class TestTarget(TemporaryShowyourworkRepository, ShowyourworkRepositoryActions):
+# TODO: Add assertions that the output files exist
+class TestTargetRule(TemporaryShowyourworkRepository, ShowyourworkRepositoryActions):
     """The setting up and building the repo with target rule"""
 
     def customize(self):
@@ -11,3 +12,44 @@ class TestTarget(TemporaryShowyourworkRepository, ShowyourworkRepositoryActions)
 
     def build_local(self):
         super().build_local(args=["generate_data"])
+
+
+class TestTargetFile(TemporaryShowyourworkRepository, ShowyourworkRepositoryActions):
+    """The setting up and building the repo with target rule"""
+
+    def customize(self):
+        self.add_pipeline_script()
+
+        self.add_pipeline_rule()
+
+    def build_local(self):
+        super().build_local(args=["src/data/test_data.npz"])
+
+
+class TestTargetMulti(TemporaryShowyourworkRepository, ShowyourworkRepositoryActions):
+    """The setting up and building the repo with target rule"""
+
+    def customize(self):
+        self.add_pipeline_script()
+
+        self.add_pipeline_rule()
+
+        with open(self.cwd / "Snakefile", "a") as f:
+            print("\n", file=f)
+            print(
+                "\n".join(
+                    [
+                        "rule touchfile:",
+                        "    output:",
+                        "        'src/data/afile.txt'",
+                        "    shell:",
+                        "        '''",
+                        "        touch {output}",
+                        "        '''",
+                    ]
+                ),
+                file=f,
+            )
+
+    def build_local(self):
+        super().build_local(args=["generate_data", "touchfile"])
