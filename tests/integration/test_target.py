96c3807e593c611edb48b58109a57445d6511ad9
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-03-28 12:45:37 -0400

    Support targets in `showyourwork build` (#690)
    * update build_local to support CLI arguments
    
    * Add failing integration test with a target rule
    
    * Filter out target argumnent from snakemake preprocessing
    
    * Add integration test with two target rules
    
    * Add asserts in `check_build()`
    
    * Add changelog entry

---
+++ tests/integration/test_target.py
@@ -0,0 +1,65 @@
+from helpers import ShowyourworkRepositoryActions, TemporaryShowyourworkRepository
+
+
+# TODO: Add assertions that the output files exist
+class TestTargetRule(TemporaryShowyourworkRepository, ShowyourworkRepositoryActions):
+    """The setting up and building the repo with target rule"""
+
+    def customize(self):
+        self.add_pipeline_script()
+
+        self.add_pipeline_rule()
+
+    def build_local(self):
+        super().build_local(args=["generate_data"])
+
+    def check_build(self):
+        assert (self.cwd / "src/data/test_data.npz").is_file()
+
+
+class TestTargetFile(TemporaryShowyourworkRepository, ShowyourworkRepositoryActions):
+    """The setting up and building the repo with target rule"""
+
+    def customize(self):
+        self.add_pipeline_script()
+
+        self.add_pipeline_rule()
+
+    def build_local(self):
+        super().build_local(args=["src/data/test_data.npz"])
+
+    def check_build(self):
+        assert (self.cwd / "src/data/test_data.npz").is_file()
+
+
+class TestTargetMulti(TemporaryShowyourworkRepository, ShowyourworkRepositoryActions):
+    """The setting up and building the repo with target rule"""
+
+    def customize(self):
+        self.add_pipeline_script()
+
+        self.add_pipeline_rule()
+
+        with open(self.cwd / "Snakefile", "a") as f:
+            print("\n", file=f)
+            print(
+                "\n".join(
+                    [
+                        "rule touchfile:",
+                        "    output:",
+                        "        'src/data/afile.txt'",
+                        "    shell:",
+                        "        '''",
+                        "        touch {output}",
+                        "        '''",
+                    ]
+                ),
+                file=f,
+            )
+
+    def build_local(self):
+        super().build_local(args=["generate_data", "touchfile"])
+
+    def check_build(self):
+        assert (self.cwd / "src/data/test_data.npz").is_file()
+        assert (self.cwd / "src/data/afile.txt").is_file()
